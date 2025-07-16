2c979842d458e6b0070bee84e4b3cde84b52e48f
gs1780 <mrsingh1780@gmail.com>
2025-07-16 19:18:36 +0530

    Add logout flow and display tokens on home page

--- AADConnect/Controllers/HomeController.cs
+++ AADConnect/Controllers/HomeController.cs
@@ -1,14 +1,35 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 namespace AADConnect.Controllers
 {
     public class HomeController : Controller
     {
         [AllowAnonymous]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            if (User.Identity?.IsAuthenticated ?? false)
+            {
+                var idToken = await HttpContext.GetTokenAsync("id_token");
+                var accessToken = await HttpContext.GetTokenAsync("access_token");
+                ViewData["IdToken"] = idToken ?? string.Empty;
+                ViewData["AccessToken"] = accessToken ?? string.Empty;
+            }
+
             return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            var callbackUrl = Url.Action("Index", "Home", values: null, protocol: Request.Scheme);
+            return SignOut(new AuthenticationProperties { RedirectUri = callbackUrl },
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                OpenIdConnectDefaults.AuthenticationScheme);
         }
     }
 }
