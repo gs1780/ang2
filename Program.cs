def48e4a7453ab9240c81b9ac2215a5437a4e3e9
gs1780 <mrsingh1780@gmail.com>
2025-07-16 18:11:55 +0530

    Add Swagger and landing page link

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,8 +30,16 @@ builder.Services.AddAuthorization(options =>
 builder.Services.AddScoped<IAuthorizationHandler, RoleAuthorizationHandler>();
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Auth API", Version = "v1" });
+});
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
@@ -40,5 +49,8 @@ app.UseAuthorization();
 app.UseRoleClaims();
 
 app.MapControllers();
+app.MapGet("/", () => Results.Content(
+    "<html><body><a href=\"/swagger/index.html\">Swagger UI</a></body></html>",
+    "text/html"));
 
 app.Run();
